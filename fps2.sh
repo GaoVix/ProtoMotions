87727a522774a19bb9843458fae148745887b160
Weixiang Gao <gaowx2003@gmail.com>
2026-03-03 12:33:31 +0800

    f extra

---
+++ fps2.sh
@@ -0,0 +1,30 @@
+# python3.10 fps.py \
+#     --no-clip \
+#     --output-path /mnt/projects/protomotions/fps3 \
+#     --smpl-path /mnt/projects/dataset/amass \
+#     --motion-list /mnt/projects/protomotions/ProtoMotions/lists/b3.txt 
+
+python3.10 fps2.py \
+    --no-clip \
+    --output-path /mnt/projects/protomotions/fps22 \
+    --smpl-path /mnt/projects/dataset/amass \
+    --motion-list /mnt/projects/protomotions/ProtoMotions/lists/all.txt 
+
+# python3.10 fps.py \
+#     --no-clip \
+#     --output-path /mnt/projects/protomotions/fps1 \
+#     --smpl-path /mnt/projects/dataset/amass \
+#     --motion-list /mnt/projects/protomotions/ProtoMotions/lists/b1.txt 
+
+# python3.10 fps.py \
+#     --no-clip \
+#     --output-path /mnt/projects/protomotions/fps0 \
+#     --smpl-path /mnt/projects/dataset/amass \
+#     --motion-list /mnt/projects/protomotions/ProtoMotions/lists/b0.txt 
+
+
+# python3.10 fps.py \
+#     --no-clip \
+#     --output-path /mnt/projects/protomotions/supp2 \
+#     --smpl-path /mnt/projects/dataset/amass \
+#     --motion-list /mnt/projects/protomotions/ProtoMotions/lists/supp2.txt 
